86370aa43999777244b798155050cd19c53618ce
VIJAY <pvijaydevanand@gmail.com>
2026-02-23 11:06:18 +0530

    Add prime number checking functionality
    Implement a method to check if a number is prime.

---
+++ Math/PrimeNumber/solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class tempCodeRunnerFile { 
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(0)); 
+    }
+}
