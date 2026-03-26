bf2542f92505b49ab2f912dd5b3066684293758e
VIJAY <176230432+pesalavijay@users.noreply.github.com>
2026-03-26 19:57:18 +0530

    Add optimal solution for continuous subarray sum

---
+++ Arrays/3.Prefix Sum/ContinuousSubarraySum/optimal.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int prefixSum = 0;
+        for(int i=0; i<nums.length; i++){
+            prefixSum += nums[i];
+            int remainder = prefixSum % k;
+            if(map.containsKey(remainder)){
+                if(i- map.get(remainder) > 1){
+                    return true;
+                }
+            }else{
+                map.put(remainder, i);
+            }
+        }
+        return false;
+        
+    }
+}
