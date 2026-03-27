259bdbcb49df38c5df06057a3c06bd8ae466534e
VIJAY <176230432+pesalavijay@users.noreply.github.com>
2026-03-27 09:51:47 +0530

    Add betterApproach.java for subarray sum problem
    Implement a solution to count subarrays divisible by k using a nested loop approach.

---
+++ Arrays/3.Prefix Sum/SubarraySumDivisiblebyK/betterApproach.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int count= 0;
+        for(int i=0; i<nums.length; i++){
+            int sum = 0;
+            for(int j=i; j<nums.length; j++){
+                sum += nums[j];
+                if(sum % k == 0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+even this is giving the TLE error 
