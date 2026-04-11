dc950fa5a3c93febcc90c262367c4820d8842079
VIJAY <176230432+pesalavijay@users.noreply.github.com>
2026-04-12 00:00:39 +0530

    Update optimal.java

--- Strings/1.TwoPointers/ValidPalindrome II/optimal.java
+++ Strings/1.TwoPointers/ValidPalindrome II/optimal.java
@@ -2,19 +2,22 @@ class Solution {
     public boolean validPalindrome(String s) {
         int left = 0, right = s.length()-1;
         while(left < right){
-            if(s.charAt(left) == s.charAt(right)){
-                left++;
-                right--;
-            } else{
+            if(s.charAt(left) != s.charAt(right)){
                 return checkPalindrome(s, left+1, right) || checkPalindrome(s, left, right-1);
             }
+            left++;
+            right--;
         }
         return true;
     }
 
-    private boolean checkPalindrome(String s, int left, int right){
+    public boolean checkPalindrome(String s, int left, int right){
         while(left < right){
-            if(s.charAt(left++) != s.charAt(right--)) return false;
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
         }
         return true;
     }
