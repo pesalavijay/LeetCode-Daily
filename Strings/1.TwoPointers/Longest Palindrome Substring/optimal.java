24073fe917d05528032d91033b9e5736a81b248b
VIJAY <176230432+pesalavijay@users.noreply.github.com>
2026-04-13 23:22:45 +0530

    Optimize longest palindrome substring algorithm
    Optimize the longest palindrome substring algorithm by adding a condition to break the loop early.

--- Strings/1.TwoPointers/Longest Palindrome Substring/optimal.java
+++ Strings/1.TwoPointers/Longest Palindrome Substring/optimal.java
@@ -4,9 +4,12 @@ class Solution {
         int start = 0, end = 0;
 
         for(int i=0; i<s.length(); i++){
+            if((s.length()-i)*2 < (end-start+1)) break;
+
             int len1 = expandAroundCenter(s, i, i);
             int len2 = expandAroundCenter(s,i, i+1);
             int len = Math.max(len1, len2);
+
             if(len > end-start){
                 start = i-(len-1)/2;
                 end = i+len/2;
