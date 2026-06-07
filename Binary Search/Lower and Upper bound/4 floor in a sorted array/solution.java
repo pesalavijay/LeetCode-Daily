79f88717c5dd84ff9568c3c46f51091cf521b2f4
VIJAY <176230432+pesalavijay@users.noreply.github.com>
2026-06-07 13:43:12 +0530

    largest element ie less than target

--- Binary Search/Lower and Upper bound/4 floor in a sorted array/solution.java
+++ Binary Search/Lower and Upper bound/4 floor in a sorted array/solution.java
@@ -2,18 +2,13 @@ class Solution {
     public int findFloor(int[] arr, int x) {
         int low = 0;
         int high = arr.length - 1;
-        int ans = -1; // Default if no floor exists
-        
+        int ans = -1; 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            
-            // If the element is less than or equal to x, 
-            // it's a valid floor candidate.
             if (arr[mid] <= x) {
-                ans = mid;       // Save the index
-                low = mid + 1;   // Look right for a larger/tighter floor
+                ans = mid;
+                low = mid + 1;
             } else {
-                // Element is too big, look left
                 high = mid - 1;
             }
         }
