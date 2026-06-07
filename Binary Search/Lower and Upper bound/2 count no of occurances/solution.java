9bcce2baf226c67001dcdfa3263a73ce62660670
VIJAY <176230432+pesalavijay@users.noreply.github.com>
2026-06-07 14:56:02 +0530

    Change findFirst and findLast methods to public
    Updated method access modifiers for findFirst and findLast to public for broader accessibility.

--- Binary Search/Lower and Upper bound/2 count no of occurances/solution.java
+++ Binary Search/Lower and Upper bound/2 count no of occurances/solution.java
@@ -1,8 +1,6 @@
 class Solution {
     public int countFrequency(int[] nums, int target) {
         int firstOccurrence = findFirst(nums, target);
-        
-        // If the element doesn't exist, frequency is 0
         if (firstOccurrence == -1) {
             return 0; 
         }
@@ -12,7 +10,7 @@ class Solution {
         return lastOccurrence - firstOccurrence + 1;
     }
 
-    private int findFirst(int[] nums, int target) {
+    public int findFirst(int[] nums, int target) {
         int low = 0, high = nums.length - 1;
         int ans = -1;
         
@@ -20,8 +18,8 @@ class Solution {
             int mid = low + (high - low) / 2;
             
             if (nums[mid] == target) {
-                ans = mid;       // Record potential answer
-                high = mid - 1;  // Keep searching LEFT for an earlier occurrence
+                ans = mid; 
+                high = mid - 1; 
             } else if (nums[mid] < target) {
                 low = mid + 1;
             } else {
@@ -31,7 +29,7 @@ class Solution {
         return ans;
     }
 
-    private int findLast(int[] nums, int target) {
+    public int findLast(int[] nums, int target) {
         int low = 0, high = nums.length - 1;
         int ans = -1;
         
@@ -39,8 +37,8 @@ class Solution {
             int mid = low + (high - low) / 2;
             
             if (nums[mid] == target) {
-                ans = mid;       // Record potential answer
-                low = mid + 1;   // Keep searching RIGHT for a later occurrence
+                ans = mid;      
+                low = mid + 1;
             } else if (nums[mid] < target) {
                 low = mid + 1;
             } else {
